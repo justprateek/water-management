c6ec2135cd172e03844b6931c68d88a326b9a7b5
Prateek Mishra <justwriteprateek@gmail.com>
2021-05-02 11:27:24 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import './12.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="heading">
+      
+        <h1>Water Management System</h1>
+        <h2>Consumer Records Portal</h2>
+      
+
     </div>
+
+    <div id="logIn">
+        <h4>LOG IN</h4>
+    </div>
+
+      
+
+      <div className="vision">
+        <h3>Vision</h3>
+        <p>Enabling the Bottom of Pyramid through Water Management Services on the Cloud, Water Quality Tools, Blockchain Network and Open Source Spreadsheet, PACS. Remote Water Monitoring, Sewage Management, Quality Assurance and Recyclability of Water </p>
+      </div>
+
+      <div id="third">
+      <h3>Procedure</h3>
+         <div className="steps">
+        
+        <h5>User login</h5>
+        </div>
+        <div className="steps">
+        
+        <h5 id="asdf">Water Consumption Meters Data Collection</h5>
+        </div>
+        <div className="steps">
+       
+        <h5>Bill Generation</h5>
+        </div>
+        <div className="steps">
+        
+        <h5>Bill Payment</h5>
+        </div>
+
+
+      </div>
+
+
+      
+    </div>
+
   );
 }
 
